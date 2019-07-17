0ab8517380e70d8a4f1b669bf5693bd39ee4998e
Md. Samiul Alam <samiulalam555@gmail.com>
2019-07-17 14:18:06 +0600

    Use hard coded mongo uri to connect mongoose

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const config = require('./config');
 
 // connect to mongo db
-const mongoUri = config.mongoUri;
+const mongoUri = config.mongoUri || `mongodb+srv://halum:aq10bEGf7ordiWYT@cluster0-sdiyd.mongodb.net/test?retryWrites=true&w=majority`;
 mongoose.connect(mongoUri, { useNewUrlParser: true });
 mongoose.connection.on('error', () => {
   throw new Error(`unable to connect to database: ${mongoUri}`);
